c4bc5e3c592dc1c1ae9b94823b81652c98d8dad8
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-01-05 11:56:22 +0700

    Update minor changes to http -> https

--- resources/views/layouts/lay_account.blade.php
+++ resources/views/layouts/lay_account.blade.php
@@ -8,22 +8,15 @@
         <!-- START @META SECTION -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <meta name="description" content="Blankon is a theme fullpack admin template powered by Twitter bootstrap 3 front-end framework. Included are multiple example pages, elements styles, and javascript widgets to get your project started.">
-        <meta name="keywords" content="admin, admin template, bootstrap3, clean, fontawesome4, good documentation, lightweight admin, responsive dashboard, webapp">
-        <meta name="author" content="Djava UI">
         <title>{{$title}}</title>
         <!--/ END META SECTION -->
 
         <!-- START @FAVICONS -->
-        {{--<link href="http://themes.djavaui.com/blankon-fullpack-admin-theme/img/ico/html/apple-touch-icon-144x144-precomposed.png" rel="apple-touch-icon-precomposed" sizes="144x144">--}}
-        {{--<link href="http://themes.djavaui.com/blankon-fullpack-admin-theme/img/ico/html/apple-touch-icon-114x114-precomposed.png" rel="apple-touch-icon-precomposed" sizes="114x114">--}}
-        {{--<link href="http://themes.djavaui.com/blankon-fullpack-admin-theme/img/ico/html/apple-touch-icon-72x72-precomposed.png" rel="apple-touch-icon-precomposed" sizes="72x72">--}}
-        {{--<link href="http://themes.djavaui.com/blankon-fullpack-admin-theme/img/ico/html/apple-touch-icon-57x57-precomposed.png" rel="apple-touch-icon-precomposed">--}}
         <link href="{{$assetUrl}}/images/shortcut icon.png" rel="shortcut icon">
         <!--/ END FAVICONS -->
 
         <!-- START @FONT STYLES -->
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet">
         <!--/ END FONT STYLES -->
 
         <!-- START @GLOBAL MANDATORY STYLES -->
@@ -136,16 +129,16 @@
         <!--/ END JAVASCRIPT SECTION -->
 
         <!-- START GOOGLE ANALYTICS -->
-        <script>
-            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        {{--<script>--}}
+            {{--(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){--}}
+                {{--(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),--}}
+                    {{--m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)--}}
+            {{--})(window,document,'script','//www.google-analytics.com/analytics.js','ga');--}}
 
-            ga('create', 'UA-55892530-1', 'auto');
-            ga('send', 'pageview');
+            {{--ga('create', 'UA-55892530-1', 'auto');--}}
+            {{--ga('send', 'pageview');--}}
 
-        </script>
+        {{--</script>--}}
         <!--/ END GOOGLE ANALYTICS -->
     </body>
     <!-- END BODY -->
